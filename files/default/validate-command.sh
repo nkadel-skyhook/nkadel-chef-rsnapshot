0b3d65714fb83a8014b64c879bf2a77adda678a9
Nico Kadel-Garcia <nkadel@skyhookwireless.com>
2014-11-18 14:41:06 -0500

    Insist on 'rsync --server-sender' for read-only access.

--- files/default/validate-command.sh
+++ files/default/validate-command.sh
@@ -4,9 +4,14 @@ case "$SSH_ORIGINAL_COMMAND" in
   *\&*|*\(*|*\{*|*\;*|*\<*|*\`*)
     echo "Connection closed."
   ;;
-  rsync\ --server*)
+  # Read-only
+  rsync\ --server\ --sender*)
     sudo $SSH_ORIGINAL_COMMAND
   ;;
+  ## permitted write, not appropriate for rsnapshot
+  #rsync\ --server*)
+  #  sudo $SSH_ORIGINAL_COMMAND
+  #;;
   *)
     echo "Connection closed."
   ;;
