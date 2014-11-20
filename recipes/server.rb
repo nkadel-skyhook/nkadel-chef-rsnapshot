ee52659a56bbfc678ec453e0e7d462f84b00dba7
Nico Kadel-Garcia <nkadel@skyhookwireless.com>
2014-11-20 11:18:15 -0500

    Clear up 'command' for key generation.

--- recipes/server.rb
+++ recipes/server.rb
@@ -16,12 +16,7 @@ keyfile = node['rsnapshot']['server']['keydir'] + node['rsnapshot']['server'['ke
 execute "create ssh keypair for rsnapshot" do
   cwd rsnapshot_keydir
   user "root"
-  command <<-KEYGEN.gsub(/^ +/, '')
-ssh-keygen -t dsa -b 2048 -f #{keyfile} -N '' \
-  -C 'root@#{node['fqdn']}-#{Time.now.strftime('%FT%T%z')}' && \
-  chmod 0600 #{keyfile} && \
-  chmod 0644 #{keyfile}.pub
-KEYGEN
+  command "sh-keygen -t dsa -b 2048 -f #{keyfile} -N '' -C 'root@#{node['fqdn']}-#{Time.now.strftime('%FT%T%z')}' && chmod 0600 #{keyfile} && chmod 0644 #{keyfile}.pub"
   creates "#{keyfile}"
 end
 
