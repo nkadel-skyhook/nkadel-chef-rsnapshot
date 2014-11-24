653da23a4331aa7b0a7b265827b06e649fd24098
Nico Kadel-Garcia <nkadel@skyhookwireless.com>
2014-11-24 18:06:45 -0500

    Update attribute handling, especially for fomer 'keydir' option.

--- attributes/server.rb
+++ attributes/server.rb
@@ -6,7 +6,7 @@ default['rsnapshot']['server']['keydir'] = '/etc/rsnapshot/.ssh'
 default['rsnapshot']['server']['keyname'] = 'rsnapshot_dsa'
 # The contents of the public key shipped to the clients
 # This is overwritten on each invocation of the server recipe!
-default['rsnapshot']['server']['ssh_key'] = nil
+default['rsnapshot']['server']['ssh_key'] = ''
 
 default['rsnapshot']['server']['intervals']['hourly'] = 2
 default['rsnapshot']['server']['intervals']['daily'] = 7
--- recipes/server.rb
+++ recipes/server.rb
@@ -4,29 +4,30 @@ package "rsnapshot"
 require 'shellwords'
 
 # create the key directory as needed
-directory "#{rsnapshot_keydir}" do
+directory node['rsnapshot']['server']['keydir'] do
   owner "root"
   group "root"
   mode "0700"
+  recursive true
 end
 
-keyfile = node['rsnapshot']['server']['keydir'] + node['rsnapshot']['server']['keyname']
+keyfile = node['rsnapshot']['server']['keydir'] + '/' + node['rsnapshot']['server']['keyname']
 
 # create the private key if necessary
 execute "create-ssh-keypair-for-rsnapshot" do
-  cwd rsnapshot_keydir
+  cwd node['rsnapshot']['server']['keydir']
   user "root"
-  command "sh-keygen -t dsa -b 2048 -f #{keyfile} -N '' -C 'root@#{node['fqdn']}-#{Time.now.strftime('%FT%T%z')}' && chmod 0600 #{keyfile} && chmod 0644 #{keyfile}.pub"
+  command "ssh-keygen -t dsa -f #{keyfile} -N '' -C 'root@#{node['fqdn']}-#{Time.now.strftime('%FT%T%z')}'"
   creates "#{keyfile}"
 end
 
-ruby_block "set-public-key-to-node" do
-  block do
-    unless Chef::Config[:solo]
-      node['rsnapshot']['server']['ssh_key'] = File.read("#{keyfile}.pub").strip
-    end
-  end
-end
+#ruby_block "set-public-key-to-node" do
+#  block do
+#    unless Chef::Config[:solo]
+#      node['rsnapshot']['server']['ssh_key'] = File.read("#{keyfile}.pub").strip
+#    end
+#  end
+#end
 
 directory node['rsnapshot']['server']['snapshot_root'] do
   owner "root"
@@ -42,33 +43,33 @@ node['rsnapshot']['server']['clients'].each_pair do |fqdn, paths|
   end
 end
 
-search(:node, "roles:#{node['rsnapshot']['client_role']}") do |client|
-  paths = client['rsnapshot']['client']['paths']
-  next unless paths && paths.any?
-
-  paths.each do |path|
-    path = path.end_with?("/") ? Shellwords.escape(path) : "#{Shellwords.escape(path)}/"
-    if client.name == node.name
-      backup_targets << "#{path}\t#{client['fqdn']}/"
-    else
-      # FIXME: What about ipv6?
-      backup_targets << "#{client['rsnapshot']['client']['username']}@#{client['ipaddress']}:#{path}\t#{client['fqdn']}/"
-    end
-  end
-end
-
-template node['rsnapshot']['server']['config_file'] do
-  source "rsnapshot.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables "backup_targets" => backup_targets,
-            "ssh_key_location" => "#{keyfile}"
-end
-
-template "/etc/cron.d/rsnapshot" do
-  source "cron.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-end
+#search(:node, "roles:#{node['rsnapshot']['client_role']}") do |client|
+#  paths = client['rsnapshot']['client']['paths']
+#  next unless paths && paths.any?
+#
+#  paths.each do |path|
+#    path = path.end_with?("/") ? Shellwords.escape(path) : "#{Shellwords.escape(path)}/"
+#    if client.name == node.name
+#      backup_targets << "#{path}\t#{client['fqdn']}/"
+#    else
+#      # FIXME: What about ipv6?
+#      backup_targets << "#{client['rsnapshot']['client']['username']}@#{client['ipaddress']}:#{path}\t#{client['fqdn']}/"
+#    end
+#  end
+#end
+#
+#template node['rsnapshot']['server']['config_file'] do
+#  source "rsnapshot.conf.erb"
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#  variables "backup_targets" => backup_targets,
+#            "ssh_key_location" => "#{keyfile}"
+#end
+#
+#template "/etc/cron.d/rsnapshot" do
+#  source "cron.erb"
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#end
