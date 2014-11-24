e7494498255b2e314f0618c66cb73382353fc4e7
Nico Kadel-Garcia <nkadel@skyhookwireless.com>
2014-11-24 18:10:27 -0500

    Add sudo dependency and more supported operating systems to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,13 @@ maintainer_email "info@finn.de"
 license          "MIT"
 description      "rsnapshot"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
-%w{ ubuntu debian}.each do |os|
+%w{ ubuntu debian centos fedora scientific amazon }.each do |os|
   supports os
 end
 
 depends "cron"
 depends "openssh"
 depends "rsync"
+depends "sudo"
